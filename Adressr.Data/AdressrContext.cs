0814108473523d6bd4d15da20138cd0b3b4c18dc
Raymond Zhu <raymondzhu1995@gmail.com>
2026-09-15 00:23:49 -0400

    Add Basic Entity Framework Dependencies
    Nugeted some basic entity framework packages and setup the basic code for the DB context to later use for setting up Entity Framework with the SQL Server DB.

---
+++ Adressr.Data/AdressrContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Adressr.Data
+{
+    public class AdressrContext: DbContext
+    {
+        public AdressrContext(DbContextOptions<AdressrContext> option) : base(option)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
--- AdressrNET/Program.cs
+++ AdressrNET/Program.cs
@@ -1,10 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Adressr.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReact", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200").AllowAnyHeader().AllowAnyMethod().AllowCredentials();
+    });
+});
+
+builder.Services.AddDbContext<AdressrContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
+
+app.UseCors("AllowReact");
+
+
 
 // Configure the HTTP request pipeline.
 
