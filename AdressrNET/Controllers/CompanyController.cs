18e97efc7d928e620322fade73de4542a9ba13a0
Raymond Zhu <raymondzhu1995@gmail.com>
2026-09-14 21:45:27 -0400

    Initial Build
    Literally nothing much, just started the project and added a starting controller. Test the Github Action build.

---
+++ AdressrNET/Controllers/CompanyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdressrNET.Controllers
+{
+    public class CompanyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
