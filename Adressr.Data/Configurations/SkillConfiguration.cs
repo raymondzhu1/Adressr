d7fdeffbbe0cec8dbb76d0cccfed504afff197ca
Raymond Zhu <raymondzhu1995@gmail.com>
2026-09-24 19:44:34 -0400

    Skill Model
    Add Skill Model, though it's not completely done yet.

---
+++ Adressr.Data/Configurations/SkillConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Adressr.Data.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Adressr.Data.Configurations
+{
+    public class SkillConfiguration : IEntityTypeConfiguration<Skill>
+    {
+        public void Configure(EntityTypeBuilder<Skill> builder)
+        {
+            builder.HasKey(x => x.SkillID);
+
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(20);
+        }
+    }
+}
---
+++ Adressr.Data/Model/Skill.cs
@@ -0,0 +1,11 @@
+﻿using Adressr.Library.Interfaces;
+
+namespace Adressr.Data.Model
+{
+    public class Skill : ISkill
+    {
+        public int SkillID { get; set; }
+
+        public required string Name { get; set; }
+    }
+}
